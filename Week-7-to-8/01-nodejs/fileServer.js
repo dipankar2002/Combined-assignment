69e0306ab61c76956d25a056aa375078778c3101
dipankar2002 <dipankarganguly15@gmail.com>
2026-04-20 11:08:49 +0000

    done

--- Week-7-to-8/01-nodejs/fileServer.js
+++ Week-7-to-8/01-nodejs/fileServer.js
@@ -17,5 +17,36 @@ const fs = require('fs');
 const path = require('path');
 const app = express();
 
+app.get('/files', (req, res) => {
+  const directoryPath = path.join(__dirname, 'files');
+  fs.readdir(directoryPath, (err, files) => {
+    if (err) {
+      return res.status(500).json({ error: 'Unable to scan directory' });
+    }
+    res.status(200).json(files);
+  });
+});
+
+app.get('/files/:filename', (req, res) => {
+  const filename = req.params.filename;
+  const filePath = path.join(__dirname, 'files', filename);
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if(err) {
+      if (err.code === 'ENOENT') {
+        return res.status(404).send('File not found');
+      }
+      return res.status(500).json({ error: 'Unable to read file' });
+    }
+    res.status(200).send(data);
+  });
+});
+
+app.use((req, res) => {
+  res.status(404).send('Not Found');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
 
 module.exports = app;
